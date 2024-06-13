367d50430ace8c31a141a732795a3e90193aa76d
luishenriquetp <luishenriquetp@gmail.com>
2024-06-13 17:44:55 -0300

    ProductSliders component added

--- src/api/getAll.ts
+++ src/api/getAll.ts
@@ -42,6 +42,48 @@ export const getProducts = (): Promise<Product[]> => {
     });
 };
 
+export const getBrandNewModels = (): Promise<Product[]> => {
+  return fetch('api/brandNewModels.json')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Failed to fetch products: ${response.statusText}`);
+      }
+      return response.json();
+    })
+    .then(data => data.slice(0, 12) as Product[])
+    .catch(error => {
+      throw error;
+    });
+};
+
+export const getHotPrices = (): Promise<Product[]> => {
+  return fetch('api/hotPrices.json')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Failed to fetch products: ${response.statusText}`);
+      }
+      return response.json();
+    })
+    .then(data => data.slice(0, 12) as Product[])
+    .catch(error => {
+      throw error;
+    });
+};
+
+export const getYouMayAlsoLike = (): Promise<Product[]> => {
+  return fetch('api/youMayAlsoLike.json')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Failed to fetch products: ${response.statusText}`);
+      }
+      return response.json();
+    })
+    .then(data => data.slice(0, 12) as Product[])
+    .catch(error => {
+      throw error;
+    });
+};
+
 export const getTablets = (): Promise<Tablet[]> => {
   return fetch('api/tablets.json')
     .then(response => {
