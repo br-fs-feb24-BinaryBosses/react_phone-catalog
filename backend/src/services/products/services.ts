2c09c9fdd29353b523ec0b73041c078169f9c439
Allanpt <jatallantavares@gmail.com>
2024-07-18 10:44:25 -0300

    handle colors with SPACE like 'space gray' to 'space-gray'

--- backend/src/services/products/services.ts
+++ backend/src/services/products/services.ts
@@ -14,6 +14,40 @@ class ProductServices {
       include: { description: true },
     });
 
+    if (!allData) {
+      return null;
+    }
+
+    const { colorsAvailable } = allData;
+
+    for (let i = 0; i < colorsAvailable.length; i += 1) {
+      const splitedColor = colorsAvailable[i].split(" ");
+
+      if (splitedColor.length === 2) {
+        const findIndexColor = colorsAvailable.findIndex(
+          (el) => el === colorsAvailable[i],
+        );
+
+        colorsAvailable.splice(findIndexColor, 1);
+
+        colorsAvailable.push(splitedColor.join("-"));
+      }
+    }
+
+    let { color } = allData;
+
+    const splitedColor = color.split(" ");
+
+    if (splitedColor.length === 2) {
+      color = splitedColor.join("-");
+    }
+
+    console.log(color);
+    console.log(colorsAvailable);
+    
+    allData.color = color;
+    allData.colorsAvailable = colorsAvailable;
+
     return allData;
   }
 }
