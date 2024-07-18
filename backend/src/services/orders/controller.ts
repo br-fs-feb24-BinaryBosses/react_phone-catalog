4d5dd5c172f774c2e4cc80383e1f0f25c637d9ee
Matheus Stefan <roccomatheus0@gmail.com>
2024-07-17 21:44:17 -0300

    implement of cartPage endPoints

--- backend/src/services/orders/controller.ts
+++ backend/src/services/orders/controller.ts
@@ -10,34 +10,58 @@ ordersRouter.post(
   "/insertOrder",
   authMiddleware,
   async (req: Request & { userId?: string }, res) => {
-    const createdOrder = await ordersService.createOrder({
-      status: "active",
-      userId: req.userId!,
-    });
+    const { userId, status, token } = req.body;
 
-    StandardResponse.responseWrapper({
-      message: "ok",
-      res,
-      data: createdOrder,
-      statusCode: 200,
-    });
-  },
+    if (!userId || !status || !token) {
+      return res.status(404).json({ message: "Fields can't be empty!" });
+    }
+    try {
+      const createdOrder = await ordersService.createOrder({
+        status: "active",
+        userId: req.userId!,
+      });
+
+      return StandardResponse.responseWrapper({
+        message: "ok",
+        res,
+        data: createdOrder,
+        statusCode: 200,
+      });
+    } catch (error) {
+      return res.status(500).json({ mesage: "Erro ao criar carrinho" });
+    }
+  }
 );
 
 ordersRouter.post(
   "/insertItemInOrder",
   authMiddleware,
   async (req: Request & { userId?: string }, res) => {
-    const data = req.body;
-    const itemInserted = await ordersService.insertItemInOrder(data);
+    const { orderId, productId, quantity } = req.body;
 
-    StandardResponse.responseWrapper({
-      message: "ok",
-      res,
-      data: itemInserted,
-      statusCode: 200,
-    });
-  },
+    if (!orderId || !productId || !quantity) {
+      return res.status(404).json({ message: "Fields can't be empty" });
+    }
+
+    try {
+      const itemInserted = await ordersService.insertItemInOrder({
+        orderId,
+        productId,
+        quantity,
+      });
+
+      return StandardResponse.responseWrapper({
+        message: "ok",
+        res,
+        data: itemInserted,
+        statusCode: 200,
+      });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ mesage: "Erro ao adicionar item ao carrinho" });
+    }
+  }
 );
 
 ordersRouter.get(
@@ -46,15 +70,137 @@ ordersRouter.get(
   async (req: Request & { userId?: string }, res) => {
     const { userId } = req.body;
 
-    const allOrderFromUser = await ordersService.getAllOrders(userId);
+    if (!userId) {
+      return res.status(404).json({ message: "Order not found!" });
+    }
 
-    StandardResponse.responseWrapper({
-      message: "ok",
-      res,
-      data: allOrderFromUser,
-      statusCode: 200,
-    });
-  },
+    try {
+      const allOrderFromUser = await ordersService.getAllOrders(userId);
+
+      return StandardResponse.responseWrapper({
+        message: "ok",
+        res,
+        data: allOrderFromUser,
+        statusCode: 200,
+      });
+    } catch (error) {
+      return res.status(500).json({ mesage: "Erro ao buscar ordem" });
+    }
+  }
+);
+
+ordersRouter.delete(
+  "/deleteOrder/:id",
+  authMiddleware,
+  async (req: Request & { userId?: string }, res) => {
+    const { id } = req.params;
+
+    if (!id) {
+      return res.status(400).json({ message: "Invalid Order ID format" });
+    }
+
+    try {
+      const deletedOrder = await ordersService.deleteOrder(id);
+
+      return StandardResponse.responseWrapper({
+        message: "Order deleted successfully",
+        res,
+        data: deletedOrder,
+        statusCode: 204,
+      });
+    } catch (error) {
+      return res.status(500).json({ message: "Erro ao deletar carrinho" });
+    }
+  }
+);
+
+ordersRouter.delete(
+  "/deleteOrderItem/:id",
+  authMiddleware,
+  async (req: Request & { userId?: string }, res) => {
+    const { id } = req.params;
+
+    if (!id) {
+      return res
+        .status(404)
+        .json({ message: "Necessário passar o id do item a ser deletado!" });
+    }
+
+    try {
+      const deletedItem = await ordersService.deleteOrderItem(id);
+      return StandardResponse.responseWrapper({
+        message: "Order item deleted successfully",
+        res,
+        data: deletedItem,
+        statusCode: 204,
+      });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ message: "Erro ao deletar item do carrinho" });
+    }
+  }
+);
+
+ordersRouter.patch(
+  "/updateOrderStatus/:id",
+  authMiddleware,
+  async (req: Request & { userId?: string }, res) => {
+    const { id } = req.params;
+    const { status } = req.body;
+
+    // Validate status
+    if (!status || !["active", "closed"].includes(status)) {
+      return res
+        .status(400)
+        .json({ message: "Field 'status' should not be null" });
+    }
+
+    try {
+      const updatedOrder = await ordersService.updateOrder(id, {
+        status,
+      });
+
+      return StandardResponse.responseWrapper({
+        message: "Order status updated successfully",
+        res,
+        data: updatedOrder,
+        statusCode: 200,
+      });
+    } catch (error) {
+      return res.status(500).json({ mesage: "Erro ao atualizar o carrinho" });
+    }
+  }
+);
+
+ordersRouter.patch(
+  "/updateOrderItem/:id",
+  authMiddleware,
+  async (req: Request & { userId?: string }, res) => {
+    const { id } = req.params;
+    const { quantity } = req.body;
+
+    if (!quantity) {
+      return res
+        .status(404)
+        .json({ message: "Should have at least 1 quantity to update" });
+    }
+
+    try {
+      const updatedItem = await ordersService.updateOrderItem(id, { quantity });
+
+      return StandardResponse.responseWrapper({
+        message: "Order status updated successfully",
+        res,
+        data: updatedItem,
+        statusCode: 200,
+      });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ message: "Erro ao atualizar o item do carrinho" });
+    }
+  }
 );
 
 export default ordersRouter;
--- backend/src/services/orders/services.ts
+++ backend/src/services/orders/services.ts
@@ -24,9 +24,19 @@ class OrdersService {
     orderId: string;
     productId: string;
     quantity: number;
-    totalPrice: number;
   }) {
-    const { orderId, productId, quantity, totalPrice } = item;
+    const { orderId, productId, quantity } = item;
+
+    const product = await prisma.product.findUnique({
+      where: { id: productId },
+    });
+
+    if (!product) {
+      throw new AppError("Product not found", 404);
+    }
+
+    const totalPrice = product.priceRegular * quantity;
+
     try {
       const createdItem = await prisma.orderItem.create({
         data: {
@@ -50,6 +60,76 @@ class OrdersService {
     });
 
     return allOrders;
+  }
+
+  async deleteOrder(id: string) {
+    try {
+      await prisma.orderItem.deleteMany({
+        where: {
+          orderId: id,
+        },
+      });
+      const deletedOrder = await prisma.order.delete({
+        where: { id },
+      });
+
+      return deletedOrder;
+    } catch {
+      throw new AppError("Erro ao deletar a ordem", 404);
+    }
+  }
+
+  async deleteOrderItem(id: string) {
+    try {
+      const deletedOrderItem = await prisma.orderItem.delete({
+        where: { id },
+      });
+
+      return deletedOrderItem;
+    } catch {
+      throw new AppError("Erro ao deletar o item da ordem", 404);
+    }
+  }
+
+  async updateOrder(orderId: string, updatedData: any) {
+    try {
+      const updatedOrder = await prisma.order.update({
+        where: { id: orderId },
+        data: updatedData,
+      });
+
+      return updatedOrder;
+    } catch {
+      throw new AppError("Erro ao atualizar o carrinho", 404);
+    }
+  }
+
+  async updateOrderItem(id: string, updatedData: { quantity: number }) {
+    try {
+      const orderItem = await prisma.orderItem.findUnique({
+        where: { id },
+        include: { product: true },
+      });
+
+      if (!orderItem) {
+        throw new AppError("Order item not found", 404);
+      }
+
+      const productPrice = orderItem.product.priceRegular;
+      const totalPrice = productPrice * updatedData.quantity;
+
+      const updatedOrderItem = await prisma.orderItem.update({
+        where: { id },
+        data: {
+          quantity: updatedData.quantity,
+          totalPrice,
+        },
+      });
+
+      return updatedOrderItem;
+    } catch {
+      throw new AppError("Erro ao atualizar o item do carrinho", 404);
+    }
   }
 }
 
