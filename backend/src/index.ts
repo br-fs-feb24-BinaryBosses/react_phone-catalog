fba3f3a064ad7d1a5ed39506014c484399d6797d
Rafael Wesley <151199792+rwlp@users.noreply.github.com>
2024-07-19 16:21:10 -0300

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../frontend_static/dist')));
 
-app.get('/', (req, res) => {
+app.get('', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend_static/dist', 'index.html'));
 });
 
